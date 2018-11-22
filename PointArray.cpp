2a5465f9c6a965c787910afe98d3f55ed2c8bad9
Iangabo <31105000+Iangabo@users.noreply.github.com>
2018-11-22 09:19:43 -0500

    Update PointArray.cpp

--- PointArray.cpp
+++ PointArray.cpp
@@ -1,47 +1,109 @@
-#ifndef POINTARRAY_H
-#define POINTARRAY_H
+#include <iostream>
+#include <iostream>
+#include "PointArray.h"
 
-#include "Point.h"
+using namespace std;
+
 template<typename TP>
-class PointArray
+PointArray<TP>::PointArray(){
+    this->size=0;
+    points=new Point1<TP>[size];
+}
+template<typename TP>
+PointArray<TP>::PointArray(const Point1<TP> pts[],const int size){
+    this->size=size;
+    points=new Point1<TP>[size];
+    for(int y=0;y<size;y++)
+        points[y]=pts[y];
+}
+template<typename TP>
+PointArray<TP>::PointArray(PointArray &o){
+    this->size=o.size;
+    points=new Point1<TP>[size];
+    for(int y=0;y<size;y++)
+        points[y]=o.points[y];
+}
+template<typename TP>
+PointArray<TP>::~PointArray(){
+    delete[] points;
+}
+template<typename TP>
+void PointArray<TP>::resize(int newsize){
+    Point1<TP> *ptr=new Point1<TP>[newsize];
+    int minsize=(newsize>size)?size:newsize;
+    for(int y=0;y<minsize;y++)
+        ptr[y]=points[y];
+    delete[] points;
+    size=newsize;
+    points=ptr;
+}
+template<typename TP>
+void PointArray<TP>::print(){
+    for(int y=0;y<size;y++)
+        points[y].print();
+}
+template<typename TP>
+void PointArray<TP>::clear(){
+    resize(0);
+}
+template<typename TP>
+void PointArray<TP>::push_back(const Point1<TP> &p){
+    resize(size+1);
+    points[size-1]=p;
+}
+
+template<typename TP>
+void PointArray<TP>::insert(const int pos,const Point1<TP> &p){
+    resize(size+1);
+    for(int y=(size-1);pos-1<y;y--)
+        points[y]=points[y-1];
+    points[pos-1]=p;
+}
+template<typename TP>
+void PointArray<TP>::remove(const int pos){
+    for(int y=pos-1;y<size-1;y++)
+        points[y]=points[y+1];
+    size--;
+    resize(size);
+}
+
+template<typename TP>
+int PointArray<TP>::getSize(){
+    return size;
+}
+
+
+template <typename TP>
+PointArray<TP> PointArray<TP>::operator+(const PointArray& _d)
 {
-    public:
-        PointArray();
-        PointArray(const Point<TP> pts[],const int size);
+    PointArray<TP> result =*this;
+    int a=0;
+    for(int i=0;i<sizeof(_d);i++);
+    {
+        bool add=true;
+        for (int j=0;j<size;j++)
+        {
+            if (_d.points[a] == points[j])
+                add=false;
+        }
+        if (add==true)
+            result.push_back(_d.points[a]);
+        a++;
+    }
+    return result;
 
-        PointArray(const PointArray<TP>&);
-        PointArray(PointArray &o);
-        ~PointArray();
-        void print();
-        void clear();
-        void push_back(const Point<TP> &p);
+}
 
-        void insert(const int,const Point<TP> &p);
-        void remove(const int);
-        int getSize();
-
-        PointArray<TP> operator+(const PointArray&);
-
-       friend ostream& operator<<(ostream &o, const PointArray<TP>& _d)
-	    {
-		if ( _d.size == 0 )
-		{
-			cout<<"()";
-			return o;
-		}
-
-		for(int i = 0; i < _d.size-1; i++)
-			cout<<_d.points[i]<<", ";
-		cout<<_d.points[_d.size-1];
-
-		return o;
-	    }
-
-
-    private:
-        int size;
-
-        Point<TP> *points;
-        void resize(int);
-};
-#endif // POINTARRAY_H
+/*
+template<typename TP>
+int PointArray<TP>::txt(){
+    stringstream ss;
+    ss<<name<<".txt";
+    string s=ss.str();
+     arch.open(s.data(),ios::in);
+     while (!arch.eof())
+    {
+        getline(arch,texto);
+        cout<<texto<<endl;
+    }
+}*/
